589b0e56c592951358a3c55fb1e2922488d256d2
Neeyee-xy <110258515+Neeyee-xy@users.noreply.github.com>
2022-09-15 07:27:01 +0100

    adding

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -3,9 +3,10 @@
  * print_alphabet - prints the alphabet, in lowercase, followed by a new line
  */
 
-vold main(vold){
-void print_alphabet(void);
-
+int main(void)
+{
+    print_alphabet();
+    return (0);
 }
 void print_alphabet(void){
 	char ch;
