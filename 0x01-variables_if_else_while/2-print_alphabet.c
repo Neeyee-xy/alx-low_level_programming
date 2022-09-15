a4c2a6c31657b5ecf696cc3dba7ebc074a19dead
Neeyee-xy <110258515+Neeyee-xy@users.noreply.github.com>
2022-09-15 06:58:24 +0100

    adding

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -6,21 +6,13 @@
  */
 int main(void)
 {
-	int n, m;
-
-	for (n = 48; n <= 57; n++)
-	{
-		for (m = 48; m <= 57; m++)
+	char alph=''a';
+		while(alpha <='z')
 		{
-			putchar(n);
-			putchar(m);
-			if (n != 57 || m != 57)
-			{
-				putchar(',');
-				putchar(' ');
-			}
+			putchar('%c',alpha);
+			alpha ++;
+			putchar('\n');
 		}
-	}
-	putchar('\n');
+		
 	return (0);
 }
