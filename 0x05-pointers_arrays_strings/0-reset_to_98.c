115d8eb4909fb77e516a79a0b57fe16bcc05f494
Neeyee-xy <110258515+Neeyee-xy@users.noreply.github.com>
2022-09-21 04:11:37 +0100

    adding

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,7 +1,11 @@
 #include "main.h"
-/** 
-  *this os fxn 98
-  */
+	/**
+	 * adding fxn 98
+	 */
+/**
+ * print_arg - Prints a string using printf
+ * @arg: The string to be printed
+ */
 void reset_to_98(int *n)
 {
   *n= 98;
